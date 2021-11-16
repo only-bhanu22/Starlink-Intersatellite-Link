23dd12c0182c975a8c33492d29acc09495236b02
Bhanupratap Rathore <80579174+superbhanu22@users.noreply.github.com>
2021-11-16 21:19:43 +0530

    Add files via upload

---
+++ Starlink/src/Classes/LEOsatellite.java
@@ -0,0 +1,37 @@
+package Classes;
+
+import Colors.Colors;
+
+public class LEOsatellite extends Satellite {
+
+    private int finalPosition ; 
+    private int LEOsatelliteID ;
+
+    public LEOsatellite(int finalPosition , int LEOsatelliteID){
+        this.finalPosition = finalPosition ; 
+        this.LEOsatelliteID = LEOsatelliteID ;
+    }
+
+    @Override
+    public int sendMessage() {
+        if((finalPosition == 2*LEOsatelliteID) || (finalPosition==2*LEOsatelliteID+1)){
+            System.out.println(Colors.RED+"LEOsatellite "+LEOsatelliteID+" thread : "+Colors.GREEN+"This is LEOsatellite "+LEOsatelliteID+" sending message to GroundStation "+finalPosition+Colors.RESET);
+
+            return -1 ; 
+        }
+        else if(finalPosition == 2*LEOsatelliteID+2 || finalPosition == 2*LEOsatelliteID+3 || finalPosition == 2*LEOsatelliteID-1 || finalPosition == 2*LEOsatelliteID-3){
+            System.out.println(Colors.RED+"LEOsatellite "+LEOsatelliteID+" thread : "+Colors.GREEN+"This is LEOsatellite "+LEOsatelliteID+" sending message to LEOsatellite "+(LEOsatelliteID+1)+Colors.RESET);
+            return 0 ;
+        }
+        else{
+            System.out.println(Colors.RED+"LEOsatellite "+LEOsatelliteID+" thread : "+Colors.GREEN+"This is LEOsatellite "+LEOsatelliteID+" sending message to GSOsatellite "+Colors.RESET);
+            return 1 ;
+        }
+    }
+
+    @Override
+    public void receiveMessage() {
+        
+    }
+
+}
---
+++ Starlink/src/Driver.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+import Colors.Colors;
+import Classes.GroundStation; 
+import Classes.LEOsatellite; 
+import Classes.GSOsatellite;
+
+
+public class Driver {
+
+    static int numberOfMessages ;
+    
+    public static void main(String[] args){
+        System.out.println(Colors.PURPLE+"Enter the number of messages you want to send from one groundstation station to another"+Colors.RESET);
+        Scanner scr = new Scanner(System.in)  ; 
+        numberOfMessages = scr.nextInt() ; 
+
+        while(numberOfMessages -->0){
+            int initialGroundStation = scr.nextInt() ; 
+            int finalGroundStation = scr.nextInt() ; 
+
+            GroundStation groundStation = new GroundStation(initialGroundStation , finalGroundStation);
+            LEOsatellite [] leoSatellites = new LEOsatellite[5] ;
+
+            int transferToLEO = groundStation.sendMessage(); 
+
+            leoSatellites[transferToLEO] = new LEOsatellite(finalGroundStation , transferToLEO);
+
+            int transferFromLEO = leoSatellites[transferToLEO].sendMessage();
+
+            if(transferFromLEO==-1){
+                groundStation.receiveMessage();
+            }
+            
+            if(transferFromLEO == 0){
+                leoSatellites[transferFromLEO+1] = new LEOsatellite(finalGroundStation, transferToLEO+1) ;
+                leoSatellites[transferFromLEO+1].sendMessage();
+
+                groundStation.receiveMessage();
+            }
+
+            if(transferFromLEO == 1){
+                 GSOsatellite gsoSatellite = new GSOsatellite(finalGroundStation) ;
+                 transferToLEO = gsoSatellite.sendMessage();
+
+                 leoSatellites[transferFromLEO]=new LEOsatellite(finalGroundStation, transferToLEO);
+                 leoSatellites[transferFromLEO].sendMessage();
+                 
+                 groundStation.receiveMessage();
+            }
+            
+            System.out.println();
+            
+        }
+
+
+        scr.close();
+    }
+}
