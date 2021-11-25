60279c641dbfc8372ca49f075ffb56e8d2854776
Bhanupratap Rathore <80579174+superbhanu22@users.noreply.github.com>
2021-11-25 16:45:24 +0530

    Delete Starlink directory

--- Starlink/src/Classes/Constellation.java
+++
@@ -1,7 +0,0 @@
-package Classes;
-
-public class Constellation {
-    Constellation(){
-        // System.out.println("This is the constellation which contains all the starlink satellites");
-    }
-}
