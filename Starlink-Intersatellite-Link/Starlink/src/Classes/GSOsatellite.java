66d07cd9874e41dc06c1c9afdae56c190a6b8124
Bhanupratap Rathore <80579174+superbhanu22@users.noreply.github.com>
2021-11-25 16:47:57 +0530

    Add files via upload
    Changed Repository Structure

---
+++ Starlink-Intersatellite-Link/Starlink/src/Classes/GSOsatellite.java
@@ -0,0 +1,24 @@
+package Classes;
+
+import Colors.Colors;
+
+public class GSOsatellite extends Satellite implements Runnable {
+
+    private int finalPosition ; 
+
+    @Override
+    public void run() {
+        System.out.println("GSO Satellite thread created");
+    }
+
+    public GSOsatellite(int finalPosition){
+        this.finalPosition = finalPosition ; 
+    }
+    
+    @Override
+    public int sendMessage() {
+        System.out.println(Colors.RED+"GSOsatellite 0 thread : "+Colors.GREEN+"This is GSOsatellite 0. sending message to LEOSatellite "+finalPosition/2+Colors.RESET );
+        return finalPosition/2 ; 
+    }
+
+}
---
+++ Starlink-Intersatellite-Link/Starlink/src/Classes/Satellite.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public abstract class Satellite extends Constellation implements CommunicationInterface {
+    Satellite(){
+        // System.out.println("This is the satellite a small part of constellation which contains one GSO-Satellite and five LEO-Satellite");
+    }
+}
+
