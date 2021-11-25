e9a26d15cc75e79eb5ecaf69daea55deb12f0f32
Bhanupratap Rathore <80579174+superbhanu22@users.noreply.github.com>
2021-11-25 22:58:10 +0530

    Add files via upload
    final commit

---
+++ starlink/Starlink-Intersatellite-Link/Starlink-Intersatellite-Link/Starlink/src/Classes/Constellation.java
@@ -0,0 +1,7 @@
+package Classes;
+
+public abstract class Constellation {
+    Constellation(){
+        // System.out.println("This is the constellation which contains all the starlink satellites");
+    }
+}
